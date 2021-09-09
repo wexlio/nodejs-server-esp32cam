9c83986c22dd861ca5b911e2bd10a655f7411df4
andresarango730 <andresarango.es@gmail.com>
2021-09-09 18:37:56 -0500

    make it better

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const url = require('url');
 
 const WebSocket = require('ws');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const express_config= require('./config/express.js');
 
